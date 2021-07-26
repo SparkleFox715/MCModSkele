892527457ab83308ea372df59659ff266d885b28
SparkleFox715 <jupiter.am705@gmail.com>
2021-07-26 05:20:14 -0500

    hello

--- src/Block.java
+++ src/Block.java
@@ -1,2 +1,46 @@
+import java.util.*;
 public class Block {
+    int XCord,YCord, ZCord;
+    int Btype;//0->air, 1->ore, 2 ->other
+    int Otype;//0->coal, 1->iron,2->gold,3->redstone, 4->lapiz, 5->diamond, 6->emerald, 7->neither
+    public String lineColor;
+    HashMap<Integer, String> BlockType = new HashMap<>();
+    HashMap<Integer, String> OreType = new HashMap<>();
+    public Block(int Btype, int OType, int X,int Y, int Z){
+        this.Btype = Btype;
+        this.Otype = OType;
+        XCord = X;
+        YCord = Y;
+        ZCord = Z;
+        BlockType.put(0, "air");
+        BlockType.put(1, "ore");
+        BlockType.put(2, "other");
+        OreType.put(0, "coal");
+        OreType.put(1, "iron");
+        OreType.put(2, "gold");
+        OreType.put(3, "redstone");
+        OreType.put(4, "lapiz");
+        OreType.put(5, "diamond");
+        OreType.put(6, "emerald");
+        OreType.put(7, "player");
+        lineColor="";
+    }
+    public int[] getCords(){
+        return new int[]{XCord, YCord, ZCord};
+    }
+    public double calcDist(Block b){
+        //calculate horizontal distance
+        int[] Bcords = b.getCords();
+        double dist = Math.sqrt(Math.pow(XCord-Bcords[0], 2)+Math.pow(ZCord-Bcords[2], 2));
+        //calculate vertical distance
+        dist = Math.sqrt(Math.pow(dist, 2)+Math.pow(YCord-Bcords[1], 2));
+        return dist;
+    }
+    public String toString(){
+        return BlockType.get(Btype)+" "+OreType.get(Otype)+" "+XCord+" "+YCord+" "+ZCord+" "+getPlayerDist();
+    }
+    public double getPlayerDist(){
+        return calcDist(new Block(0,0,0,0,0));
+    }
+
 }
--- src/MCModeRunner.java
+++ src/MCModeRunner.java
@@ -1,3 +1,64 @@
+import java.util.*;
+import java.io.*;
 public class MCModeRunner {
+    public static void main(String[] args) throws IOException{
+        Block playerPos = new Block(2, 7, 0,0,0);
+        ArrayList<Block> blocks = new ArrayList();
+        int chunkw, chunkl;
+        int blockPreference;
+        Scanner in = new Scanner(new File("input.txt"));
+        boolean auto = in.nextBoolean();//false: manual, true: auto
+        chunkw = in.nextInt();
+        chunkl = in.nextInt();
+        blockPreference = in.nextInt();
+        blocks.addAll(searchOres(chunkw, chunkl, blockPreference));
+        while(in.hasNextLine()){
+            Block temp = new Block(in.nextInt(),in.nextInt(),in.nextInt(),in.nextInt(),in.nextInt());
+            blocks.add(temp);
+            System.out.println(temp.getPlayerDist());
+        }
+        System.out.println(blocks);
+        blocks = quickSort(blocks);
+        System.out.println(blocks);
+
+    }
+
+    public static ArrayList<Block> searchOres(int chunkw, int chunkl, int blockPreference){
+        ArrayList<Block> ret = new ArrayList();
+        for(int i =0;i<257;i++){
+            for(int x = 0;x<chunkw*16;x++){
+                for(int j=0;j<chunkl;j++){
+                    /*
+                    Use the Minecraft library to get the type of block at the xyz coordinate and
+                    add it to the arraylist if the block is the preferred type of ore.
+                    */
+                }
+            }
+        }
+        return ret;
+    }
+    public static ArrayList<Block> quickSort(ArrayList<Block> blocks){
+        if(blocks.size()<=1){
+            return blocks;
+        }
+        ArrayList<Block>smaller = new ArrayList();
+        ArrayList<Block>same = new ArrayList();
+        ArrayList<Block>larger = new ArrayList();
+        double pivot = blocks.get(0).getPlayerDist();
+        for(int i=0;i<blocks.size();i++){
+            if(blocks.get(i).getPlayerDist()<pivot){
+                smaller.add(blocks.get(i));
+            }else if(blocks.get(i).getPlayerDist()>pivot){
+                larger.add(blocks.get(i));
+            }else{
+                same.add(blocks.get(i));
+            }
+        }
+        smaller = quickSort(smaller);
+        larger  = quickSort(larger);
+        smaller.addAll(same);
+        smaller.addAll(larger);
+        return smaller;
+    }
 }
 
